7a6e8a42b5741c417b83688665db1690206f1054
salliyme <307623183@qq.com>
2018-11-22 23:51:59 +0800

    first commit

---
+++ src/Hello.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * PHP framework team
+ * author: liupan@salliyme.com
+ * Date: 2018/11/22 23:02
+ */
+
+namespace salliyme;
+
+
+class Hello
+{
+    const VERSION = '1.0.1';
+
+    public function say()
+    {
+        echo 'Hello, salliyme.'.PHP_EOL;
+        echo 'This message powered by salliyme.('.static::VERSION.')'.PHP_EOL;
+    }
+}
